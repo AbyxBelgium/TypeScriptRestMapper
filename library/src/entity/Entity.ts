269c16190ce7d30c15e4782b935edf57c3cb85cc
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2019-03-31 18:43:50 +0200

    Update npmignore

--- library/src/entity/Entity.ts
+++ library/src/entity/Entity.ts
@@ -22,34 +22,4 @@ export default abstract class Entity {
 
         return this[type];
     }
-
-    getStoreJson(): object {
-        if (!this["_store_properties"]) {
-            return [];
-        }
-
-        return this.getJson("_store_properties");
-    }
-
-    getUpdateJson(): object {
-        if (!this["_update_properties"]) {
-            return [];
-        }
-
-        return this.getJson("_update_properties");
-    }
-
-    private getJson(propertyType: string): object {
-        let output = {};
-
-        if (!this[propertyType]) {
-            throw "No valid properties are set to be stored or updated on this object! Please use the @Store or @Update decorators on the desired fields.";
-        }
-
-        for (let field of this[propertyType]) {
-            output[field.substr(1)] = this[field];
-        }
-
-        return output;
-    }
 }
